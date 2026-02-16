e4036ebc1af921085b272950cca2abccbefa83b5
Zeroandonehelloworld <zero.and.one.hello.world@gmail.com>
2026-02-16 23:36:51 +0200

    Update test_sample.js

--- test_sample.js
+++ test_sample.js
@@ -1,21 +1,16 @@
-import { remote } from 'webdriverio';
+const { remote } = require('webdriverio');
 
-const capabilities={
+const capabilities = {
   "appium:platformName": "Android",
   "appium:deviceName": "127.0.0.1:5555",
   "appium:automationName": "UiAutomator2",
-  //"appium:automationName": "UiAutomator2",
-  //"appium:app": "path/to/app.apk"
-  // "appium:appPackage": "com.facebook.katana",
-  // "appium:appActivity": "com.facebook.katana.LoginActivity" // or MainActivity
-  //"appium:appPackage": "com.google.android.youtube",
-  //"appium:appActivity": "com.google.android.youtube.HomeActivity"
+  // Uncomment and set app path if testing with APK
+  // "appium:app": "path/to/app.apk"
+
+  // Example: Play Store
   "appium:appPackage": "com.android.vending",
-//"appium:appActivity": "com.google.android.finsky.activities.MainActivity"
-
-
+  // "appium:appActivity": "com.google.android.finsky.activities.MainActivity"
 };
-
 
 (async () => {
   const driver = await remote({
@@ -26,27 +21,17 @@ const capabilities={
     capabilities: capabilities,
   });
 
-  await driver.pause(30000);
-  await driver.$('//android.widget.TextView[@text="Search apps & games"]').click();
-  await driver.$('//android.widget.EditText').setValue("Instgram");
-  await driver.pressKeyCode(66);
-  await driver.pause(10000);
-  await driver.$('//androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[1]/android.view.View[1]/android.view.View[2]/android.widget.Button').click();
-await driver.saveScreenshot('./screenshot.png');
-await driver.deleteSession();
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
+  try {
+    await driver.pause(3000); // wait for app to load
+    await driver.$('//android.widget.TextView[@text="Search apps & games"]').click();
+    await driver.$('//android.widget.EditText').setValue("Instagram");
+    await driver.pressKeyCode(66); // press Enter
+    await driver.pause(10000); // wait for results
+    await driver.$('//androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[1]/android.view.View[1]/android.view.View[2]/android.widget.Button').click();
+    await driver.saveScreenshot('./screenshot.png');
+  } catch (err) {
+    console.error("Test error:", err);
+  } finally {
+    await driver.deleteSession();
+  }
 })();
